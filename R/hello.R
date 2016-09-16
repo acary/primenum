102a82b18dff0297e6b88361baba8471c03d502e
Andy Cary <andy@andycary.com>
2016-09-16 15:08:16 -0700

    Initial commit
    A simple program

---
+++ R/hello.R
@@ -0,0 +1,33 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+prime1 = c(2, 3, 5, 7, 11)
+
+prime2 = c(13, 17, 19, 23, 29)
+
+prime3 = c(31, 37, 41, 43, 47)
+
+prime4 = c(53, 59, 61, 67, 71)
+
+prime5 = c(73, 79, 83, 89, 97)
+
+prime1[2]
+prime2[4]
+
+(prime1[2]*prime2[4])*(prime1[2]*prime2[4])
